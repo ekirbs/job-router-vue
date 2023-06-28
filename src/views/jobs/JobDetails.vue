bf428ee8cc834be145653733cfc7440d75f6e669
Eric K <erickirberger@gmail.com>
2023-06-28 16:48:32 -0400

    router fetches to db.json

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,13 @@
 <template>
-  <h1>Job Details Page</h1>
-  <!-- <p>The job id is {{ $route.params.id }}</p> -->
-  <p>The job id is {{ id }}</p>
-  <!-- no longer need the rest because I put it in data -->
-  <div>
-
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- <p>The job id is {{ $route.params.id }}</p> -->
+    <p>The job id is {{ id }}</p>
+    <!-- no longer need the rest because I put it in data -->
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
   </div>
 </template>
 
@@ -16,6 +19,17 @@
     //     id: this.$route.params.id
     //   }
     // }
+    data() {
+      return {
+        job: null
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    }
   }
 </script>
 
